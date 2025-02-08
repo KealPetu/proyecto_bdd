81079f2f35f88813bbc92c679573b1be4fc91ae5
Scorguss <brayanleomaldonado@gmail.com>
2025-02-07 20:45:13 -0500

    Vista de Tablas y Datos/ Master

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 import cx_Oracle
+
 
 app = Flask(__name__)
 app.secret_key = 'clave123'  # Necesario para usar mensajes flash
@@ -30,13 +31,35 @@ def login():
     return render_template('login.html')
 
 # Ruta para el modo Master
-@app.route('/master')
+################ SOLO ESTO SE MUEVE PARA LO QUE TIENE QUE VER CON CONSULTAS
+@app.route('/master', methods=['GET', 'POST'])
 def master():
-    # Intentar conectar a la base de datos master
     connection = conectar_master()
     if connection:
-        connection.close()  # Cerrar la conexión después de verificar
-        return render_template('master.html')
+        cursor = connection.cursor()
+        cursor.execute("SELECT table_name FROM user_tables WHERE table_name NOT LIKE 'AUDITORIA_%'")
+        tablas = [row[0] for row in cursor.fetchall()]
+        ###METODO 
+
+        datos = None
+        columnas = None
+        tabla_seleccionada = None
+
+        if request.method == 'POST':
+            # Si se seleccionó una tabla, mostrar los datos
+            tabla_seleccionada = request.form.get('tabla')
+            if tabla_seleccionada:
+                cursor.execute(f"SELECT * FROM {tabla_seleccionada}")
+                columnas = [desc[0] for desc in cursor.description]  # Obtener nombres de columnas
+                datos = cursor.fetchall()  # Obtener los datos de la tabla seleccionada
+                cursor.close()
+                connection.close()
+                return render_template('master.html', tablas=tablas, datos=datos, columnas=columnas, tabla_seleccionada=tabla_seleccionada)
+
+
+        cursor.close()
+        connection.close()
+        return render_template('master.html', tablas=tablas)
     else:
         flash("Error: No se pudo conectar a la base de datos master.", "error")
         return redirect(url_for('login'))
@@ -44,7 +67,6 @@ def master():
 # Ruta para el modo Remote
 @app.route('/remote')
 def remote():
-    # Intentar conectar a la base de datos remote
     connection = conectar_remote()
     if connection:
         connection.close()  # Cerrar la conexión después de verificar
